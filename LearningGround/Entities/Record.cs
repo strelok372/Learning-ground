3a266e1e1ab4169552a1eb343c944bbf2469ab03
vladimir-dozorov <Vladimir.Dozorov@emergn.com>
2020-03-18 01:57:39 +0300

    creating base ui, investigating data-binding

---
+++ LearningGround/Entities/Record.cs
@@ -0,0 +1,18 @@
+﻿namespace LearningGround.Entities
+{
+    public class Record
+    {
+        public string Title { get; set; }
+
+        public string Tag { get; set; }
+
+        public string Body { get; set; }
+
+        public Record(string title, string tag, string body)
+        {
+            Title = title;
+            Tag = tag;
+            Body = body;
+        }
+    }
+}
--- LearningGround/MainWindow.xaml.cs
+++ LearningGround/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LearningGround.Entities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,35 @@ namespace LearningGround
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Record> records;
+
         public MainWindow()
         {
             InitializeComponent();
+            FillData();
+        }
+
+        public void OpenRecord(object sender, SelectionChangedEventArgs e)
+        {
+            tabControl.Items.Add("123");
+        }
+
+        private void FillData()
+        {
+            tv_menu.ItemsSource = new[] { "Детское", "Подростковое", "Взрослое" };
+
+            records = new ObservableCollection<Record>
+            {
+                new Record("Заголовок 1", "Животные", "Абвгд"),
+                new Record("Заголовок 2", "Животные", "Абвгд"),
+                new Record("Заголовок 3", "Животные", "Абвгд"),
+                new Record("Заголовок 4", "Фрукты", "Абвгд"),
+                new Record("Заголовок 5", "Фрукты", "Абвгд"),
+                new Record("Заголовок 6", "Цветы", "Абвгд"),
+                new Record("Заголовок 7", "Цветы", "Абвгд"),
+            };
+
+            lv_search_result.ItemsSource = records;
         }
     }
 }
